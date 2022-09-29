a5cff5a240183198d9ff4ac703dd4794a067c8c7
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2022-09-29 15:30:10 +0200

    Update macchina.ino

--- silvia_oggetti/005_MACCHINA/macchina.ino
+++ silvia_oggetti/005_MACCHINA/macchina.ino
@@ -1,11 +1,11 @@
-#define R1 2
-#define R2 3
-#define R3 4
-#define R4 7
-#define R5 8
-#define R6 9
-#define R7 10
-#define R8 11
+#define RR1 7
+#define RR2 8
+#define RR3 3
+#define RR4 4
+#define RR5 9
+#define RR6 10
+#define RR7 11
+#define RR8 2
 
 #define SERVO1 5
 #define SERVO2 6
@@ -18,14 +18,14 @@ Servo myservo2;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(R1,OUTPUT);
-  pinMode(R2,OUTPUT);
-  pinMode(R3,OUTPUT);
-  pinMode(R4,OUTPUT);
-  pinMode(R5,OUTPUT);
-  pinMode(R6,OUTPUT);
-  pinMode(R7,OUTPUT);
-  pinMode(R8,OUTPUT);
+  pinMode(RR1,OUTPUT);
+  pinMode(RR2,OUTPUT);
+  pinMode(RR3,OUTPUT);
+  pinMode(RR4,OUTPUT);
+  pinMode(RR5,OUTPUT);
+  pinMode(RR6,OUTPUT);
+  pinMode(RR7,OUTPUT);
+  pinMode(RR8,OUTPUT);
 
   myservo1.attach(6);
   myservo2.attach(5);
@@ -37,15 +37,16 @@ uint8_t pos2 = 92;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(R1,HIGH);
-  digitalWrite(R2,HIGH);
+  digitalWrite(RR1,HIGH);
+  digitalWrite(RR2,HIGH);
 
   delay(5000);
 
-  digitalWrite(R1,LOW);
-  digitalWrite(R2,LOW);
+  digitalWrite(RR1,LOW);
+  digitalWrite(RR2,LOW);
 
   delay(2000);
-
+ 
+  
 
 }
