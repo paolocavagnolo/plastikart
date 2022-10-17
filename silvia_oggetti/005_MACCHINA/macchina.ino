bc78880b9029356a1dc51c1fe1f665256c043cb3
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2022-10-17 16:47:58 +0200

    Update macchina.ino

--- silvia_oggetti/005_MACCHINA/macchina.ino
+++ silvia_oggetti/005_MACCHINA/macchina.ino
@@ -2,47 +2,47 @@
 
 #define RR1 7 //LUCI
 #define RR2 4 //RUOTE
-#define RR3 3 
+#define RR3 3
 #define RR4 2
 #define RR5 9 //ok
 #define RR6 8 //ok
 #define RR7 11 //ok
 #define RR8 10 //ok
 
-Servo myservo1;  
-Servo myservo2;  
+Servo myservo1;
+Servo myservo2;
 
-int pos = 0;   
+int pos = 0;
 
 void setup() {
 
-  digitalWrite(RR1,HIGH);
-  digitalWrite(RR2,HIGH);
-  digitalWrite(RR3,HIGH);
-  digitalWrite(RR4,HIGH);
-  digitalWrite(RR5,HIGH);
-  digitalWrite(RR6,HIGH);
-  digitalWrite(RR7,HIGH);
-  digitalWrite(RR8,HIGH);
+  digitalWrite(RR1, HIGH);
+  digitalWrite(RR2, HIGH);
+  digitalWrite(RR3, HIGH);
+  digitalWrite(RR4, HIGH);
+  digitalWrite(RR5, HIGH);
+  digitalWrite(RR6, HIGH);
+  digitalWrite(RR7, HIGH);
+  digitalWrite(RR8, HIGH);
 
-  pinMode(RR1,OUTPUT);
-  pinMode(RR2,OUTPUT);
-  pinMode(RR3,OUTPUT);
-  pinMode(RR4,OUTPUT);
-  pinMode(RR5,OUTPUT);
-  pinMode(RR6,OUTPUT);
-  pinMode(RR7,OUTPUT);
-  pinMode(RR8,OUTPUT);
+  pinMode(RR1, OUTPUT);
+  pinMode(RR2, OUTPUT);
+  pinMode(RR3, OUTPUT);
+  pinMode(RR4, OUTPUT);
+  pinMode(RR5, OUTPUT);
+  pinMode(RR6, OUTPUT);
+  pinMode(RR7, OUTPUT);
+  pinMode(RR8, OUTPUT);
 
-  digitalWrite(RR1,HIGH);
-  digitalWrite(RR2,HIGH);
-  digitalWrite(RR3,HIGH);
-  digitalWrite(RR4,HIGH);
-  digitalWrite(RR5,HIGH);
-  digitalWrite(RR6,HIGH);
-  digitalWrite(RR7,HIGH);
-  digitalWrite(RR8,HIGH);
-  
+  digitalWrite(RR1, HIGH);
+  digitalWrite(RR2, HIGH);
+  digitalWrite(RR3, HIGH);
+  digitalWrite(RR4, HIGH);
+  digitalWrite(RR5, HIGH);
+  digitalWrite(RR6, HIGH);
+  digitalWrite(RR7, HIGH);
+  digitalWrite(RR8, HIGH);
+
   myservo1.attach(5);
   myservo2.attach(6);
 
@@ -51,75 +51,79 @@ void setup() {
   myservo1.write(95);
   myservo2.write(95);
 
-  
-  
+
+
 }
 
 void loop() {
 
-  // FARI LAMPEGGIANTI
-//  for (uint8_t i=0; i<8; i++) {
-//    digitalWrite(RR1, LOW);
-//    delay(300);
-//    digitalWrite(RR1, HIGH);
-//    delay(150);
-//  }
-//
-//  delay(1000);
-//
-//  for (uint8_t i=0; i<8; i++) {
-//    digitalWrite(RR1, LOW);
-//    delay(300);
-//    digitalWrite(RR1, HIGH);
-//    delay(150);
-//  }
+  // WHEEL START
+  digitalWrite(RR2, LOW);
 
-  // RUOTE
-//  digitalWrite(RR2, LOW);
-//
-//  delay(2000);
-//
-//  for (uint8_t i=0; i<5; i++) {
-//    digitalWrite(RR1, LOW);
-//    delay(200);
-//    digitalWrite(RR1, HIGH);
-//    delay(500);
-//  }
-//
-//  delay(2000);
-//
-//  digitalWrite(RR2, HIGH);
-//
-//  delay(1000);
+  // LIGHTS BLINK
+  for (uint8_t i = 0; i < 4; i++) {
+    digitalWrite(RR1, LOW);
+    delay(300);
+    digitalWrite(RR1, HIGH);
+    delay(150);
+  }
 
-  //FARI FISSI
-
- // digitalWrite(RR1, LOW);
-
-  delay(2000);
-
-  for (uint8_t x=0; x < 60; x++) {
-    myservo1.write(93+x);
-    myservo2.write(92-x);
+  // DOORS OPEN
+  for (uint8_t x = 0; x < 60; x++) {
+    myservo1.write(93 + x);
+    myservo2.write(92 - x);
     delay(5);
   }
 
-  delay(2000);
+  // LIGHTS BLINK
+  for (uint8_t i = 0; i < 4; i++) {
+    digitalWrite(RR1, LOW);
+    delay(300);
+    digitalWrite(RR1, HIGH);
+    delay(150);
+  }
 
-  for (uint8_t x=60; x > 0; x--) {
-    myservo1.write(93+x);
-    myservo2.write(92-x);
+  // DOOR CLOSE
+  for (uint8_t x = 60; x > 0; x--) {
+    myservo1.write(93 + x);
+    myservo2.write(92 - x);
+    delay(25);
+  }
+  myservo1.write(93);
+  myservo2.write(92);
+
+  // WHEEL STOP
+  digitalWrite(RR2, HIGH);
+
+  delay(1000);
+
+  //LIGHTS UP
+
+  digitalWrite(RR1, LOW);
+
+  delay(1000);
+
+
+  // DOORS OPEN
+  for (uint8_t x = 0; x < 60; x++) {
+    myservo1.write(93 + x);
+    myservo2.write(92 - x);
+    delay(5);
+  }
+
+  delay(1000);
+
+
+  // DOORS CLOSE
+  for (uint8_t x = 60; x > 0; x--) {
+    myservo1.write(93 + x);
+    myservo2.write(92 - x);
     delay(25);
   }
   myservo1.write(93);
   myservo2.write(92);
 
   digitalWrite(RR1, HIGH);
-
-//
-
-
-
 
 
 }
