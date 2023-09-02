fb3915e5e7260e57196ef0287e21a6df60263bd8
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2023-09-02 20:57:20 +0200

    Update CONTROLLINO_gufo_v2.ino

--- aosta_animali/CONTROLLINO_gufo/CONTROLLINO_gufo_v2.ino
+++ aosta_animali/CONTROLLINO_gufo/CONTROLLINO_gufo_v2.ino
@@ -12,124 +12,118 @@
 #define stepEnable CONTROLLINO_D2
 
 #define esPin CONTROLLINO_A1
-#define startPin CONTROLLINO_A0
 
 AccelStepper stepper(AccelStepper::DRIVER, stepPulse, stepDir);
-
 void setup() {
 
-  delay(1000);
-  
-  pinMode(stepPulse,OUTPUT);
-  pinMode(stepDir,OUTPUT);
-  pinMode(stepEnable,OUTPUT);
-
-  digitalWrite(stepEnable,LOW);
+  pinMode(stepEnable, OUTPUT);
+  digitalWrite(stepEnable, LOW);
 
   stepper.setMaxSpeed(100);
   stepper.setAcceleration(100);
 
-  while (digitalRead(esPin)) {
-    
+  while (!digitalRead(esPin)) {
+
     stepper.moveTo(-10000);
     stepper.run();
-    
+
   }
 
   delay(1000);
 
   stepper.setCurrentPosition(0);
 
-  stepper.setMaxSpeed(200);
-  stepper.setAcceleration(200);
+  stepper.setMaxSpeed(700);
+  stepper.setAcceleration(700);
 
-  stepper.moveTo(300);
+  stepper.moveTo(370);
 
-  while(stepper.distanceToGo() != 0) {       
-    stepper.run(); 
-  } 
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
 
-  digitalWrite(stepEnable,HIGH);
+  digitalWrite(stepEnable, HIGH);
   
+
 }
+
+bool ggo = false;
+unsigned long tt = 0;
 
 void loop() {
 
-  if (digitalRead(startPin)) {
-    digitalWrite(stepEnable,LOW);
+  if ((millis() - tt) > (60000)) {
+    tt = millis();
+    ggo = true;
+  }
 
-    stepper.setMaxSpeed(100);
-    stepper.setAcceleration(100);
-    stepper.moveTo(10);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
-
-    delay(2000);
+  if (ggo) {
+    digitalWrite(stepEnable, LOW);
 
     stepper.setMaxSpeed(200);
     stepper.setAcceleration(200);
-    stepper.moveTo(700);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
+    stepper.moveTo(10);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
+
+    delay(2000);
+
+    stepper.setMaxSpeed(500);
+    stepper.setAcceleration(500);
+    stepper.moveTo(500);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
 
     delay(500);
 
 
-    stepper.setMaxSpeed(100);
-    stepper.setAcceleration(100);
-    stepper.moveTo(900);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
-
-    delay(2000);
-
     stepper.setMaxSpeed(300);
     stepper.setAcceleration(300);
-    stepper.moveTo(300);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
-
-    delay(2000);
-
-    stepper.setMaxSpeed(200);
-    stepper.setAcceleration(200);
-    stepper.moveTo(200);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
-
-    delay(4000);
-
-    stepper.setMaxSpeed(200);
-    stepper.setAcceleration(200);
-    while (digitalRead(esPin)) {
-    
-      stepper.moveTo(-10000);
+    stepper.moveTo(700);
+    while (stepper.distanceToGo() != 0) {
       stepper.run();
-      
     }
 
+    delay(2000);
+
+    stepper.setMaxSpeed(600);
+    stepper.setAcceleration(600);
+    stepper.moveTo(300);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
+
+    delay(2000);
+
+
+    stepper.setMaxSpeed(200);
+    stepper.setAcceleration(200);
+    while (!digitalRead(esPin)) {
+
+      stepper.moveTo(-10000);
+      stepper.run();
+
+    }
+    stepper.setCurrentPosition(0);
+
     delay(4000);
 
     stepper.setMaxSpeed(200);
     stepper.setAcceleration(200);
-    stepper.moveTo(300);
-    while(stepper.distanceToGo() != 0) {       
-      stepper.run(); 
-    } 
+    stepper.moveTo(370);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
 
     delay(2000);
+    ggo = false;
 
   }
   else {
-    digitalWrite(stepEnable,HIGH);
+    digitalWrite(stepEnable, HIGH);
     delay(100);
   }
 
-  
- 
 }
