70149e8caad55200716e557a6bbc3499bfe9fbc8
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2024-02-02 14:03:42 +0100

    Create TEST_CON_SENSORE.ino

---
+++ pedale_aste/TEST_CON_SENSORE.ino
@@ -0,0 +1,219 @@
+// CIAO MICHELE, QUESTE LE 2 VARIABILI CHE PUOI CONTROLLARE:
+
+long LIM_UPP = 150; // 200 sono 45 gradi
+
+// 496 - Gatto / Pedale
+// 493 - Asta 1
+// 494 - Asta 2
+// 495 - Asta 3
+const int startChannel = 493;
+
+///////////////////////////////////////////////////////////////
+// DA QUI IN POI PRIMA DI CAMBIARE CHIEDIMI
+//////////////////////////////////////////////////////////////
+
+#include <EEPROM.h>
+
+#define E_ADD 100
+
+#include "FastAccelStepper.h"
+
+#define dirPinStepper 9
+#define enablePinStepper 10
+#define stepPinStepper 8
+
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper = NULL;
+
+#include <DMXSerial.h>
+
+long LIM_LOW = 0;
+
+#define BTN_A A1
+#define IN_POS A4
+
+long dmxVal = 0;
+
+long pos = 0;
+unsigned long tDMX = 0;
+unsigned long tDebug = 0;
+
+bool btnEn = false;
+
+bool fB_A = false;
+bool fB_B = false;
+unsigned long dbB_A = 0;
+unsigned long dbB_B = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  delay(200);
+
+  long upp_buffer = 0;
+  EEPROM.get(E_ADD, upp_buffer);
+
+  if ((upp_buffer > 0) && (upp_buffer < 3200)) {
+    LIM_UPP = upp_buffer;
+  } else {
+    EEPROM.put(E_ADD, LIM_UPP);
+  }
+
+  DMXSerial.init(DMXReceiver);
+  delay(200);
+
+  engine.init();
+  stepper = engine.stepperConnectToPin(stepPinStepper);
+  delay(100);
+
+  // INITIAL ZERO
+  
+  // init motor
+  stepper->setDirectionPin(dirPinStepper, true, 100);
+  stepper->setEnablePin(enablePinStepper);
+  stepper->enableOutputs();
+
+  // set speed / acc
+  stepper->setSpeedInUs(20000);
+  stepper->setAcceleration(7000);
+  stepper->setCurrentPosition(LIM_LOW);
+
+  // enable motor
+  stepper->enableOutputs();
+
+  // start run backward
+  stepper->runBackward();
+
+  // wait for the signal
+  while (analogRead(IN_POS) > 500) {};
+
+  // stop
+  stepper->stopMove();
+  unsigned long tZ = millis();
+  while ((millis() - tZ) < 500) {};
+
+  // set position to -50
+  stepper->setCurrentPosition(LIM_LOW-50);
+
+  // go forward to 0
+  stepper->moveTo(LIM_LOW);
+  unsigned long tZ = millis();
+  while ((millis() - tZ) < 500) {};
+
+
+
+  if (analogRead(BTN_A) < 100) {
+
+    unsigned long tLoopBeat = 0;
+    unsigned long tLoopBack = 0;
+
+    bool beat = true;
+
+    // INFINITE TEST LOOP
+
+    stepper->setSpeedInUs(50);
+    stepper->setAcceleration(7000);
+
+    while (true) {
+
+      if (beat) {
+        if ((millis() - tLoopBack) > 3000) {
+          tLoopBeat = millis();
+          stepper->setAcceleration(150000);
+          stepper->applySpeedAcceleration();
+          delay(10);
+          stepper->moveTo(LIM_UPP);
+          beat = false;
+        }
+      } else {
+        if ((millis() - tLoopBeat) > 2000) {
+          tLoopBack = millis();
+          stepper->setAcceleration(7000);
+          stepper->applySpeedAcceleration();
+          delay(10);
+          stepper->moveTo(LIM_LOW);
+          beat = true;
+        }
+      }
+    }
+
+  } 
+
+  // PER ENTRARE NEL LOOP IL SEGNALE DMX DEVE ESSERE 0
+
+  dmxVal = DMXSerial.read(startChannel);
+
+  while (dmxVal > 0) {
+    dmxVal = DMXSerial.read(startChannel);
+    delay(100);
+  }
+
+}
+
+
+void loop() {
+
+  // END STOPS
+  pos = stepper->getCurrentPosition();
+
+  if ((pos > (LIM_UPP)) || (pos < (LIM_LOW))) {
+    stepper->forceStop();
+  }
+
+  // DMX
+  if ((millis() - tDMX) > 15) {
+    tDMX = millis();
+
+    dmxVal = DMXSerial.read(startChannel);
+
+    if (DMXSerial.noDataSince() > 2000) {
+      btnEn = true;
+    } else {
+      btnEn = false;
+    }
+
+    if (dmxVal == 255) {
+      stepper->setAcceleration(150000);
+    } else {
+      stepper->setAcceleration(7000);
+    }
+
+    stepper->applySpeedAcceleration();
+  }
+
+  if (btnEn) {
+
+    if (!fB_A) {
+      if ((millis() - dbB_A) > 200) {
+        if (analogRead(BTN_A) < 100) {
+          fB_A = true;
+          dbB_A = millis();
+
+          stepper->setAcceleration(1000);
+          stepper->applySpeedAcceleration();
+          delay(10);
+          stepper->moveTo(LIM_UPP);
+        }
+      }
+    } else {
+      if ((millis() - dbB_A) > 200) {
+        if (analogRead(BTN_A) > 1000) {
+          fB_A = false;
+          dbB_A = millis();
+
+          stepper->setAcceleration(1000);
+          stepper->applySpeedAcceleration();
+          delay(10);
+          stepper->moveTo(LIM_LOW);
+        }
+      }
+    }
+
+
+  } else {
+
+    stepper->moveTo(map(dmxVal, 0, 255, LIM_LOW, LIM_UPP));
+
+  }
+
+}
